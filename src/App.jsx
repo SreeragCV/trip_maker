9df6ca45c85a5378d02e82576492a8bc8545838a
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-12-10 18:11:43 +0530

    another use case of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  const stateUpdatingFunction = () =>
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const sortedplaces = sortPlacesByDistance(
         AVAILABLE_PLACES,
@@ -21,8 +26,7 @@ function App() {
       );
       setAvailablePlaces(sortedplaces);
     });
-
-  useEffect(stateUpdatingFunction, []);
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -41,6 +45,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -48,6 +60,11 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
